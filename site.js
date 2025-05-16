479f44d58b7198a42ba5ee8a1a2aeb92b274a293
João103001 <joao.franco@sptech.school>
2025-05-15 23:39:12 -0300

    tela de login crua, porem verificação para proteção feita!

--- site.js
+++ site.js
@@ -1,31 +1,36 @@
-function cadastrar() {
-    var n = nome.value
-    var e = email.value
-    var s = senha.value
-    if (e.includes('@') && e.length > 10) {
-        alert('email valido')
+function FazerLogin() {
+    var nome = document.getElementById('username').value;
+    var senha = document.getElementById('password').value;
+    var confirma = document.getElementById('password2').value;
+    if (nome.includes('@') && nome.length > 10) {
+        // alert('email valido')
     } else {
-        alert('insira um email valido')
+        alert('insira um email valido, contendo o "@ e no mínimo 10 caracteres')
     }
-    if ((s.includes('A') || s.includes('B') || s.includes('C') || s.includes('D') ||
-        s.includes('E') || s.includes('F') || s.includes('G') || s.includes('H') ||
-        s.includes('I') || s.includes('J') || s.includes('K') || s.includes('L') ||
-        s.includes('M') || s.includes('N') || s.includes('O') || s.includes('P') ||
-        s.includes('Q') || s.includes('R') || s.includes('S') || s.includes('T') ||
-        s.includes('U') || s.includes('V') || s.includes('W') || s.includes('X') ||
-        s.includes('Y') || s.includes('Z')) && (s.includes('!') || s.includes('@')
-            || s.includes('#') || s.includes('$') ||
-            s.includes('%') || s.includes('^') || s.includes('&') || s.includes('*') ||
-            s.includes('(') || s.includes(')') || s.includes('_') || s.includes('+') ||
-            s.includes('{') || s.includes('}') || s.includes('[') || s.includes(']') ||
-            s.includes(':') || s.includes(';') || s.includes('<') || s.includes('>') ||
-            s.includes(',') || s.includes('.') || s.includes('?') || s.includes('/') ||
-            s.includes('~') || s.includes('\\') || s.includes('|') || s.includes('-') ||
-            s.includes('='))) {
-alert('senha valida')
+   if ((senha.includes('A') || senha.includes('B') || senha.includes('C') || senha.includes('D') ||
+    senha.includes('E') || senha.includes('F') || senha.includes('G') || senha.includes('H') ||
+    senha.includes('I') || senha.includes('J') || senha.includes('K') || senha.includes('L') ||
+    senha.includes('M') || senha.includes('N') || senha.includes('O') || senha.includes('P') ||
+   senha.includes('Q') || senha.includes('R') || senha.includes('S') || senha.includes('T') ||
+    senha.includes('U') || senha.includes('V') || senha.includes('W') || senha.includes('X') ||
+    senha.includes('Y') || senha.includes('Z'))
+   &&
+   (senha.includes('!') || senha.includes('@') || senha.includes('#') || senha.includes('$') ||
+    senha.includes('%') || senha.includes('^') || senha.includes('&') || senha.includes('*') ||
+    senha.includes('(') || senha.includes(')') || senha.includes('_') || senha.includes('+') ||
+    senha.includes('{') || senha.includes('}') || senha.includes('[') || senha.includes(']') ||
+    senha.includes(':') || senha.includes(';') || senha.includes('<') || senha.includes('>') ||
+    senha.includes(',') || senha.includes('.') || senha.includes('?') || senha.includes('/') ||
+   senha.includes('~') || senha.includes('\\') || senha.includes('|') || senha.includes('-') ||
+    senha.includes('='))) {
+// alert('senha valida')
     } else {
-        alert('insira uma senha valida')
-    }}
-    function name(params) {
-        
+        alert('insira uma senha valida, que contenha uma letra maiúscula e um caractere especial')
+    }if (senha != confirma ) {
+        alert('a senha tem que ser a mesma inserida antes.')
+    } else {
+        alert('Login feito!')
     }
+
+}
+    
